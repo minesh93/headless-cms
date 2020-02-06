6720afe235d24c4ddb719c029ee891ba468190cc
Minesh <minesh@windows-laptop>
2020-02-06 23:04:30 +0000

    Fancy ass button.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,19 @@
 @extends('layout')
 @section('content')
 <main class="login">
-
+    <h1>Login</h1>
+    <form>
+        <div class="form-wrap">
+            <div class="input-wrap">
+                <label>Username</label>
+                <input type="text"/>
+            </div>
+            <div class="input-wrap">
+                <label>Password</label>
+                <input type="password"/>
+            </div>
+            <button>Do Nothing</button>
+        </div>
+    </form>
 </main>
 @endsection
