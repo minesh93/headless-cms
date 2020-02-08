0b09aa543a0ce96cbb26f8454075abf34939e1cb
Minesh Mitha <minesh@limesharp.net>
2020-02-08 19:13:49 +0000

    Types read and cast into global

--- app/Providers/HeadlessServiceProvider.php
+++ app/Providers/HeadlessServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use File;
+use View;
 
 class HeadlessServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,15 @@ class HeadlessServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        
+        if(File::exists(resource_path('cms/setup.json'))) {
+            $setup = File::get(resource_path('cms/setup.json'));
+            $setup = json_decode($setup);
+            $this->app->bind('headless', function() use ($setup) {
+                return $setup;
+            }, true);
+
+            View::share('headless', app('headless'));
+        }
     }
 
     /**
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,22 +1,26 @@
 <nav class="sidebar">
-    <div class="sidebar-title">Admin</div>
+    <div class="sidebar-title">Headless CMS</div>
 
     <div class="nav-section">
         <div class="section-title">Content</div>
         <ul class="nav-list">
-            <li>
-                <a>Posts</a>
-            </li>
-            <li>
-                <a>Pages</a>
-            </li>
-            <li>
-                <a>Experiment Pages</a>
-            </li>
+            @foreach ($headless->types as $key => $type)
+                <li>
+                    <a href="/content/{{$key}}">{{ $type->name_plural }}</a>
+                </li>
+            @endforeach
         </ul>
     </div>
 
     <div class="nav-section">
         <div class="section-title">Settings</div>
+        <ul class="nav-list">
+            <li>
+                <a href="#">Users</a>
+            </li>
+            <li>
+                <a href="#">Stuff</a>
+            </li>
+        </ul>
     </div>
 </nav>
